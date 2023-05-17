d68e77faec1f056215d9c8cdaae4e75cde0f8fd8
VarunK2801 <varunkapole19@gmail.com>
2023-05-17 21:24:44 +0530

    task 8React1

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,20 @@
-function ExpenseItem(){
-    return (<div>
-        <h2>ExpenseItem!</h2>
-        <h3>Food Rs 100</h3>
-        <h3>Petrol Rs 110</h3>
-        <h3>Movies Rs 300</h3>
-    </div>)
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  const expenseDate = new Date(2023, 4, 17);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
+  const LocationOfExpenditure = "Pune"
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <h2>{LocationOfExpenditure}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
