3460d898b82c6ffcbb35ba0651604e67a3e06a9d
VarunK2801 <varunkapole19@gmail.com>
2023-05-17 16:09:26 +0530

    food price

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,7 @@
 function ExpenseItem(){
     return (<div>
         <h2>ExpenseItem!</h2>
-        <h3>Food Rs 10</h3>
+        <h3>Food Rs 100</h3>
         <h3>Petrol Rs 100</h3>
         <h3>Movies Rs 300</h3>
     </div>)
