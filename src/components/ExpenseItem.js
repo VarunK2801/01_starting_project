7cacb688a5b8b30f06cad0b74ef8b3afad396495
VarunK2801 <varunkapole19@gmail.com>
2023-05-18 16:27:43 +0530

    task 10React1

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   
   return (
     <div className="expense-item">
-      <div>{props.item.date.toISOString()}</div>
-      <div className="expense-item__description">
-        <h2>{props.item.title}</h2>
-        <h2>{props.item.LocationOfExpenditure}</h2>
-        <div className="expense-item__price">${props.item.amount}</div>
-      </div>
+      <ExpenseDate date={props.item.date} />
+      <ExpenseDetails 
+        amount ={props.item.amount}
+        title ={props.item.title}
+        location ={props.item.LocationOfExpenditure} />
     </div>
   );
 }
